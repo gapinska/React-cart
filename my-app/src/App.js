97d95a59a6dcab580761fda77b832c4dcab2348c
gapinska <gapinska.mon@gmail.com>
2019-08-04 23:30:58 +0200

    button remove from cart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state= {
+    availableProducts: 7,
+    shoppingCart: 0,
+    
+  }
+
+  handleRemoveFromCart = () => {
+    this.setState( {
+      shoppingCart: this.state.shoppingCart - 1
+    } )
+  }
+  render(){
+    return(
+      <>
+      <button disabled = {this.state.shopppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
+      <span> {this.state.shoppingCart} </span>
+      <button>+</button>
+      </>
+    )
+  }
 }
+
+
+
+
 
 export default App;
