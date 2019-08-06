c3c8a6578c5e334119e9005484e16e51b7442e4e
gapinska <gapinska.mon@gmail.com>
2019-08-06 07:53:27 +0200

    buy button, opacity while 0 in shopping cart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,14 +18,28 @@ class App extends React.Component{
     this.setState({
       shoppingCart: this.state.shoppingCart + 1
     })
-  } 
+  }
+  
+  handleBuyButton = () => {
+    this.setState({
+      availableProducts: this.state.availableProducts - this.state.shoppingCart,
+      shoppingCart: 0
+      
+    })
+
+    
+  }
+
   render(){
+    
     return(
       <>
       <button  disabled={this.state.shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-      <span> {this.state.shoppingCart} </span>
+      <span style={this.state.shoppingCart === 0? {
+        opacity: 0.3
+      } : {}}> {this.state.shoppingCart} </span>
       <button disabled= {this.state.shoppingCart === this.state. availableProducts ? true: false} onClick= {this.handleAddToCart}>+</button>
-      {this.state.shoppingCart > 0 ? <button>BUY</button> : false}
+      {this.state.shoppingCart > 0 && <button onClick= {this.handleBuyButton}>BUY</button> }
       </>
     )
   }
